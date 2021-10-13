8155de85ce51ec617447f43cf4f4a138ecaf783b
Ian Calvert <ianjcalvert@gmail.com>
2021-10-13 11:44:21 +0100

    Iterate over schemas & tables

--- subgraph_extractor/cli.py
+++ subgraph_extractor/cli.py
@@ -13,7 +13,7 @@ def get_select_all_exclusive(
     )
 
     return pandas.read_sql(
-        sql=f"""SELECT 'SELECT ' || STRING_AGG(column_name, ', ') || ' FROM {table_schema}.{table_name}'
+        sql=f"""SELECT 'SELECT ' || STRING_AGG('"' || column_name || '"', ', ') || ' FROM {table_schema}.{table_name}'
             FROM information_schema.columns
             WHERE table_name = '{table_name}'
             AND table_schema = '{table_schema}'
@@ -34,6 +34,15 @@ def get_tables_in_schema(database_string, table_schema, ignored_tables=[]):
     return list(set(all_tables) - set(ignored_tables))
 
 
+def get_subgraph_schemas(database_string):
+    return pandas.read_sql(
+        f"""
+    SELECT subgraph as subgraph_id, name as table_schema from deployment_schemas
+    """,
+        con=database_string,
+    ).to_dict("records")
+
+
 @click.command()
 @click.option(
     "--database-string",
@@ -44,21 +53,26 @@ def main(database_string):
     """Connects to your database and pulls all data from all subgraphs"""
 
     # TODO: find the partitions to use
-    # TODO: find the schemas
-    table_schema = "sgd2"
-    tables = get_tables_in_schema(database_string, table_schema, ["poi2$"])
-    # We need to get the column names and have to explicity exclude some
-    for table_name in tables:
-        df = pandas.read_sql(
-            sql=get_select_all_exclusive(
-                database_string, table_schema, table_name, ["vid", "block_range"]
-            ),
-            con=database_string,
-            coerce_float=False,
-        )
-        print(df.head())
-        os.makedirs(f"data/{table_schema}", exist_ok=True)
-        df.to_parquet(f"data/{table_schema}/{table_name}.parquet")
+    for subgraph in get_subgraph_schemas(database_string):
+        table_schema = subgraph["table_schema"]
+        subgraph_id = subgraph["subgraph_id"]
+        # We can ignore this auto-created table, the others should all be
+        # explicitly created mappings
+        tables = get_tables_in_schema(database_string, table_schema, ["poi2$"])
+        # We need to get the column names and have to explicity exclude some
+        for table_name in tables:
+            df = pandas.read_sql(
+                sql=get_select_all_exclusive(
+                    database_string, table_schema, table_name, ["vid", "block_range"]
+                ),
+                con=database_string,
+                coerce_float=False,
+            )
+            basedir = os.path.join(
+                "data", f"subgraph={subgraph_id}", f"table={table_name}"
+            )
+            os.makedirs(basedir, exist_ok=True)
+            df.to_parquet(os.path.join(basedir, "data.parquet"))
 
 
 if __name__ == "__main__":
