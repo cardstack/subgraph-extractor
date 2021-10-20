27254cd7d404b17b43cf965f243334d57ef0b320
Ian Calvert <ianjcalvert@gmail.com>
2021-10-20 12:34:29 +0100

    Add partitioning

--- subgraph_extractor/cli.py
+++ subgraph_extractor/cli.py
@@ -3,21 +3,40 @@ import os
 import click
 import pandas
 
+import sqlalchemy
 
 def get_select_all_exclusive(
-    database_string, table_schema, table_name, ignored_columns=[]
+    database_string, table_schema, table_name, start_block=None, end_block=None, ignored_columns=[]
 ):
     """Returns a select all statement excluding certain columns"""
-    ignored_column_string = ", ".join(
-        f"'{column_name}'" for column_name in ignored_columns
-    )
+    if len(ignored_columns) > 0:
+        wrapped_columns = [f"'{column_name}'" for column_name in ignored_columns]
+        ignored_column_string = f"AND column_name NOT IN ({', '.join(wrapped_columns)})"
+    else:
+        ignored_column_string = ""
+    
+    where_clauses = []
+    if start_block:
+        where_clauses.append(f"block_number >= {start_block}")
+    if end_block:
+        where_clauses.append(f"block_number < {end_block}")
+    
+    if len(where_clauses) > 0:
+        where_clause = f"WHERE {' AND '.join(where_clauses)}"
+    else:
+        where_clause = ""
+
 
     return pandas.read_sql(
-        sql=f"""SELECT 'SELECT ' || STRING_AGG('"' || column_name || '"', ', ') || ' FROM {table_schema}.{table_name}'
+        sql=f"""SELECT 
+                'SELECT ' || STRING_AGG('"' || column_name || '"', ', ') || ' 
+                    FROM {table_schema}.{table_name}
+                    {where_clause} 
+                    ORDER BY block_number'
             FROM information_schema.columns
             WHERE table_name = '{table_name}'
             AND table_schema = '{table_schema}'
-            AND column_name NOT IN ({ignored_column_string})
+            {ignored_column_string}
             """,
         con=database_string,
     ).iloc[0][0]
@@ -52,7 +71,6 @@ def get_subgraph_schemas(database_string):
 def main(database_string):
     """Connects to your database and pulls all data from all subgraphs"""
 
-    # TODO: find the partitions to use
     for subgraph in get_subgraph_schemas(database_string):
         table_schema = subgraph["table_schema"]
         subgraph_id = subgraph["subgraph_id"]
@@ -61,18 +79,41 @@ def main(database_string):
         tables = get_tables_in_schema(database_string, table_schema, ["poi2$"])
         # We need to get the column names and have to explicity exclude some
         for table_name in tables:
-            df = pandas.read_sql(
-                sql=get_select_all_exclusive(
-                    database_string, table_schema, table_name, ["vid", "block_range"]
-                ),
-                con=database_string,
-                coerce_float=False,
-            )
-            basedir = os.path.join(
-                "data", f"subgraph={subgraph_id}", f"table={table_name}"
-            )
-            os.makedirs(basedir, exist_ok=True)
-            df.to_parquet(os.path.join(basedir, "data.parquet"))
+            # Assume all are workable by block like this
+            # TODO: check for this column and do a timestamped export if not
+            try:
+                limits_df = pandas.read_sql(
+                    sql=f"select min(block_number) as min_block, max(block_number) as max_block from {table_schema}.{table_name}",
+                    con=database_string,
+                    coerce_float=False)
+                min_block = int(limits_df['min_block'].iloc[0])
+                max_block = int(limits_df['max_block'].iloc[0])
+                for granularity in [32**2, 32**3]:
+                    # Floor the ranges
+                    start_block_allowed = (min_block // granularity)*granularity
+                    end_block_allowed = (max_block // granularity)*granularity
+                    for start_block in range(start_block_allowed, end_block_allowed, granularity):
+                        end_block = start_block + granularity
+                        df = pandas.read_sql(
+                            sql=get_select_all_exclusive(
+                                database_string, table_schema, table_name,
+                                start_block=start_block,
+                                end_block=end_block,
+                                ignored_columns=["vid", "block_range"]
+                            ),
+                            con=database_string,
+                            coerce_float=False,
+                        )
+                        basedir = os.path.join(
+                            "data", f"subgraph={subgraph_id}", f"table={table_name}",
+                            f"granularity={granularity}",
+                            f"start_block={start_block}",
+                            f"end_block={end_block}",
+                        )
+                        os.makedirs(basedir, exist_ok=True)
+                        df.to_parquet(os.path.join(basedir, "data.parquet"))
+            except sqlalchemy.exc.ProgrammingError as e:
+                print("skipping table", table_name, e)
 
 
 if __name__ == "__main__":
