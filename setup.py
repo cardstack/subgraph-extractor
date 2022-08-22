f6d347f6965a8be38bb76d448fb08f1e508acac3
Ian Calvert <ianjcalvert@gmail.com>
2022-08-22 12:42:53 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="subgraph_extractor",
-    version="0.1.2",
+    version="0.1.3",
     description="Pull data from graph-node databases into parquet files",
     url="http://github.com/cardstack/subgraph-extractor",
     author="Ian Calvert",
